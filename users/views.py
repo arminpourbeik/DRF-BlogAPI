0fb20c517eff39d3b571f086439ff57514ac6c63
Armin <armin.pourbeik@gmail.com>
2020-12-27 20:25:50 +0330

    Added registet view and serializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import RegisterUserSerializer
+
+
+class CustomUserCreate(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_user = serializer.save()
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+
+            return Response(
+                serializer.errors, status=status.status.HTTP_400_BAD_REQUEST
+            )
